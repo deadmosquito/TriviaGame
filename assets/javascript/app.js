a5f067f083541adf382a100b2ee90f71b186fac9
Womp Rat Bullseye <stevensorensen13@gmail,com>
2019-07-16 11:19:50 -0700

    images and refinement

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,162 +1,168 @@
 $(document).ready(function() {
+    // creating an array of options all at once and putting the answer with it.
     var options = [
         {
-            question: "Steve Rogers, aka Captain America, hails from what part of New York?",
-            choice: ["Queens", "Brooklyn", "Upper East Side", "Manhattan"],
+            question: "How big is a womp rat?",
+            choice: ["Tiny", "Not much bigger than 2 meters", "1 foot across", "30 feet"],
             answer: 1,
-            photo: "assets/images/captain.jpg"
+            photo: "assets/images/womprat.png"
         },
 
         {
-            question: "What is Black Panther's suit and Captain America's shield made out of?",
-            choice: ["Steel", "Titanium", "Unobtanium", "Vibranium"],
+            question: "Where does Obi-wan tell Luke to find Yoda?",
+            choice: ["Alderaan", "Tatooine", "Hoth", "Dagobah"],
             answer: 3,
-            photo: "assets/images/panther.jpg"
+            photo: "assets/images/dagobah.jpeg"
         },
 
         {
-            question: "What is the name of Thor's Hammer?",
-            choice: ["Mjolnir", "Stormbreaker", "Leviathan", "Hammer McHammerson"],
+            question: "Who trained Qui-gon Jinn?",
+            choice: ["Count Dooku", "Christopher Lee", "Yoda", "Obi-wan"],
             answer: 0,
             photo: "assets/images/thor.gif"
         },
 
         {
-            question: "Who owes Moon Knight money?",
-            choice: ["Thor", "Iron Man", "Dracula", "Hulk"],
+            question: "The Kessel run is measured by which unit?",
+            choice: ["Lightyears", "Miles", "Parsecs", "Seconds"],
             answer: 2,
             photo: "assets/images/moon.jpg"
         },
 
         {
-            question: "Who is the Marvel Character that has been resurrected the most?",
-            choice: ["Jean Grey", "Daredevil", "Namor", "Star Lord"],
+            question: "How long did it take to build the first death star?",
+            choice: ["20 years", "10 years", "We don't know how long it has existed", "The force made it in 1 year"],
             answer: 0,
             photo: "assets/images/jean.jpg"
         },
 
         {
-            question: "Who is the one character who has never been resurrected?",
-            choice: ["Wolverine", "Iron Man", "Captain America", "Uncle Ben"],
+            question: "What command by Emperor Palpatine brought down the Jedi Order?",
+            choice: ["Order 13", "Order 77", "Order 99", "Order 66"],
             answer: 3,
             photo: "assets/images/ben.jpg"
         },
 
         {
-            question: "Who attacked Red Skull after finding out he was a Nazi?",
-            choice: ["Wolverine", "Captain America", "Magneto", "Joker"],
+            question: "Which of these species is not furry?",
+            choice: ["Wookies", "Ewoks", "Wampas", "Hutts"],
             answer: 3,
             photo: "assets/images/joker.png"
         },
 
         {
-            question: "Who cameo'd as Loki in Thor: Ragnarock?",
-            choice: ["Ben Affleck", "Matt Damon", "Jimmy Fallon", "Jimmy Kimmel"],
+            question: "Complete the quote: 'never tell me the...'",
+            choice: ["Scores", "Odds", "Truth", "Reason"],
             answer: 1,
             photo:"assets/images/matt.png"
         },
 
         {
-            question: "How long was Loki falling in Dr.Strange's portal?",
-            choice: ["10 Mins", "20 Mins", "30 Mins", "4 Years"],
+            question: "How does Jabba sentence Han and Luke to be killed?",
+            choice: ["Firing Squad", "By the Rancor", "By the Sarlacc", "Walk the plank"],
             answer: 2,
             photo:"assets/images/lokiFalling.gif"
         },
         {
-            question: "Who is the true Strongest Avenger?",
-            choice: ["Hulk", "Thor", "Captain America", "Iron Man"],
+            question: "Complete the quote: “Fear leads to anger. Anger leads to hate. Hate leads to...”",
+            choice: ["Violence", "Suffering", "Evil", "Darkness"],
             answer: 1,
             photo: "assets/images/point.jpg"
         }];
-    
+    // more variables for the timer and counter at the end
     var correctCount = 0;
     var wrongCount = 0;
     var unanswerCount = 0;
-    var timer = 20;
+    var timer = 10;
     var intervalId;
     var userGuess ="";
     var running = false;
-    var qCount = options.length;
+    //var qCount = options.length;
     var pick;
     var index;
     var newArray = [];
     var holder = [];
 
     $("#reset").hide();
-    //Click the start button to start, obviously!
+    // begin the test
     $("#start").on("click", function (){
+        // once you start the button will hide
         $("#start").hide();
         displayQuestion();
         runTimer();
+        // this is a loop for each question
         for(var i = 0; i < options.length; i++){
+            // puts your choice on the screen
         holder.push(options[i]);
     }
-            })
+})
 
-    //Timer start
+            
+    // timer start
     function runTimer(){
+        // if its not running then it will run
         if (!running){
             intervalId = setInterval(decrement, 1000);
             running = true;
         }
     }
 
-    //Timer Countdown
+    // timer Countdown inserted into the html to decrease
     function decrement(){
         $("#timeleft").html("<h3>Time Remaining: " + timer + "</h3>");
         timer --;
 
-        //Stop Timer if it reaches 0
+        // stop timer if it reaches 0
         if (timer === 0) {
             unanswerCount++;
             stop();
-            $("#answerblock").html("<p>*snaps* Your Time Is Up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            $("#answerblock").html("<p>Your Time Is Up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
             hidepicture();
         }
     }
 
-    //Timer Stop
+    // timer stop
     function stop(){
         running = false;
         clearInterval(intervalId);
     }
 
-    //Randomly pick a question in array if not already shown
-    //Display a question and loop through and display possible answers
+    // randomly pick a question in array if not already shown so you dont start with the same questions
+    // display a question and loop through and display possible answers
     function displayQuestion(){
-        //Generate random index in array
+        // has the random function within the options
         index = Math.floor(Math.random()*options.length);
+        // can only be from the options
         pick = options[index];
 
-     //if (pick.shown) {
-        //recursive to continue to generate new index until one is chosen that has not shown in this game yet
-        //displayQuestion();
-    //} else {
-        //console.log(pick.question);
-        //iterate through answer array and display
+    
+        // in the html the question is displayed
         $("#questionblock").html("<h2>" + pick.question + "</h2>");
         for(var i = 0; i < pick.choice.length; i++){
+            // this makes the choice its own div
             var userChoice = $("<div>");
             userChoice.addClass("answerchoice");
             userChoice.html(pick.choice[i]);
-            //assign array position to it so you can check the answer
+            //assign array position to it so you can check the answer only within the parameters of i
             userChoice.attr("data-guessvalue", i);
+            // appends my new variable within the answer block
             $("#answerblock").append(userChoice);
-        }
-    //}
-    }
+            
+    //    
+    
+}
 
     $(".answerchoice").on("click", function(){
         //grab array position from userGuess
         userGuess = parseInt($(this).attr("data-guessvalue"));
-
+        // if correct, the timer stops and you get rewarded
         if (userGuess === pick.answer) {
             stop();
             correctCount++;
             userGuess="";
             $("#answerblock").html("<p>Correct!</p>");
             hidepicture();
-
+            // if wrong, the timer stops and you get marked
         } else {
             stop();
             wrongCount++;
@@ -164,25 +170,29 @@ $(document).ready(function() {
             $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
             hidepicture();
         }
+        
     })
+}
     
 
 function hidepicture(){
     $("#answerblock").append("<img src=" + pick.photo + ">");
+    // shows the photo with the answer
     newArray.push(pick);
     options.splice(index,1);
-
+// gives 10 seconds for each answer
     var hidpic = setTimeout(function() {
         $("#answerblock").empty();
-        timer = 30;
+        timer = 10;
 
     //run the score screen if all questions answered
-    if ((wrongCount + correctCount + unanswerCount) === qCount) {
+    if ((wrongCount + correctCount + unanswerCount) === options.length) {
         $("#questionblock").empty();
-        $("#questionblock").html("<h3>Game Over! Here's how you did: </h3>");
+        $("#questionblock").html("<h3>Game Over! Here's your score: </h3>");
         $("#answerblock").append("<h4> Correct: " + correctCount + "</h4>" );
         $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>");
         $("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>");
+        // shows the choice to play again
         $("#reset").show();
         correctCount = 0;
         wrongCount = 0;
@@ -193,9 +203,9 @@ function hidepicture(){
         displayQuestion();
 
     }
-    }, 3000);
+    }, 7000);
 }
-
+// hides the reset button once engaged and clears the score from the previous player
 $("#reset").on("click", function(){
     $("#reset").hide();
     $("#answerblock").empty();
